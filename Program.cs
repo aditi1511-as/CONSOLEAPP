b967ee5507445617085e71e1a0a50c1143117091
aditi1511-as <shrivastavaaditi454@gmail.com>
2021-06-19 12:49:56 +0530

    consoleapp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aditiconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World by Aditi!");
+        }
+    }
+}
